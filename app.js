1539d52479147c9204452ff5f1e7166477f6c3ca
Ban Pengfei <banpf@hyxt.com>
2016-03-23 14:31:56 +0800

    init page

--- app.js
+++ app.js
@@ -24,10 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
@@ -35,60 +35,34 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+	app.use(function (err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 var ws = require("nodejs-websocket");
 console.log("开始建立连接...")
 
-var game1 = null,game2 = null , game1Ready = false , game2Ready = false;
-var server = ws.createServer(function(conn){
+var game1 = null, game2 = null , game1Ready = false , game2Ready = false;
+var server = ws.createServer(function (conn) {
 	conn.on("text", function (str) {
-		console.log("收到的信息为:"+str)
-		if(str==="game1"){
-			game1 = conn;
-			game1Ready = true;
-			conn.sendText("success");
-		}
-		if(str==="game2"){
-			game2 = conn;
-			game2Ready = true;
-		}
-
-		if(game1Ready&&game2Ready){
-			game2.sendText(str);
-		}
-
-		conn.sendText(str)
+		console.log("收到的信息为:" + str)
+		conn.sendText(new Date()+"");
 	})
 	conn.on("close", function (code, reason) {
 		console.log("关闭连接")
@@ -96,12 +70,8 @@ var server = ws.createServer(function(conn){
 	conn.on("error", function (code, reason) {
 		console.log("异常关闭")
 	});
-}).listen(8001)
+}).listen(8001);
 console.log("WebSocket建立完毕")
-
-
-
-
 
 
 module.exports = app;

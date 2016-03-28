a21dae7274cee8bbf18c61a5ee0a77f94ce1024e
班鹏飞 <xiaobanxiaoban@qq.com>
2016-03-28 22:05:50 +0800

    添加驱动

--- app.js
+++ app.js
@@ -55,22 +55,7 @@ app.use(function (err, req, res, next) {
 });
 
 
-var ws = require("nodejs-websocket");
-console.log("开始建立连接...")
 
-var server = ws.createServer(function (conn) {
-	conn.on("text", function (str) {
-		console.log("收到的信息为:" + str)
-		conn.sendText(new Date()+"");
-	})
-	conn.on("close", function (code, reason) {
-		console.log("关闭连接")
-	});
-	conn.on("error", function (code, reason) {
-		console.log("异常关闭")
-	});
-}).listen(8001);
-console.log("WebSocket建立完毕")
 
 
 module.exports = app;
--- public/javascripts/xyz.js
+++ public/javascripts/xyz.js
@@ -1,8 +1,7 @@
 if(window.WebSocket){
-	var ws = new WebSocket('ws://localhost:8001');
+	var ws = new WebSocket('ws://192.168.1.1:8001');
 	ws.onopen = function(e){
 		console.log("连接服务器成功");
-		ws.send("game1");
 	}
 	ws.onclose = function(e){
 		console.log("服务器关闭");
@@ -11,7 +10,17 @@ if(window.WebSocket){
 		console.log("连接出错");
 	}
 	ws.onmessage = function(e,value){
-		$("#date").text(e.data);
+		var imu=$.parseJSON(e.data);
+		for(var x in imu){
+			for(var y in imu[x]){
+				var value=imu[x][y];
+				if(!isNaN(value) && typeof value !="boolean"){
+					value=parseFloat(value).toFixed(2);
+				}
+				$("[name='"+x+"_"+y+"']").val(value);
+			}
+		}
+		$("#date").text();
 	}
 }else{
 	alert("该浏览器不支持websocket，无法支持本程序，请换用其他的浏览器，推荐Chrome浏览器！");
@@ -19,7 +28,5 @@ if(window.WebSocket){
 }
 
 $(function(){
-	$("#btn").click(function(){
-		ws.send($(":text").val());
-	});
+	//ws.send($(":text").val());
 });
